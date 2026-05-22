8d95fb962572cf1c29abb50b45e6b7b3c7bd69c7
barnii77 <barnii77@barnii77.dev>
2026-05-22 08:01:10 +0200

    expose everything

--- g4b/__init__.py
+++ g4b/__init__.py
@@ -1,3 +1,7 @@
 from . import gguf
+from . import device
+from . import tensor
+from . import utils
+from . import kernels
 
-__all__ = ["gguf"]
+__all__ = ["gguf", "device", "tensor", "utils", "kernels"]
