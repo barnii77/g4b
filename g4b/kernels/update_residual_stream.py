12f09861746d95a6eadab034b7830f5732294128
barnii77 <barnii77@barnii77.dev>
2026-05-26 18:29:28 +0200

    separate input rsos and output rsos, also multiply in rmsnorm_w and write out to act_buf

--- g4b/kernels/update_residual_stream.py
+++ g4b/kernels/update_residual_stream.py
@@ -2,6 +2,7 @@ import triton
 from triton import language as tl
 from g4b.tensor import Tensor
 from g4b.kernels.utils import launch
+from kernels.memset import memset_contiguous_by_ptr
 
 
 def _cfg(
@@ -20,6 +21,9 @@ def _cfg(
         },
         num_warps=warps,
         num_stages=stages,
+        pre_hook=lambda args: memset_contiguous_by_ptr(
+            args["out_rsos_ptr"], args["out_rsos_shape0"] * args["out_rsos_shape1"], 0
+        ),
     )
 
 
@@ -55,22 +59,30 @@ def _cfg(
         "residual_shape0", "residual_shape1", "residual_shape2",
         "act_buf_shape0", "act_buf_shape1", "act_buf_shape2",
         "act_rsos_shape0", "act_rsos_shape1",
+        "out_rsos_shape0", "out_rsos_shape1",
+        "rmsnorm_w_shape0",
         "residual_stride0", "residual_stride1", "residual_stride2",
         "act_buf_stride0", "act_buf_stride1", "act_buf_stride2",
         "act_rsos_stride0", "act_rsos_stride1",
+        "out_rsos_stride0", "out_rsos_stride1",
+        "rmsnorm_w_stride0",
         # fmt: on
     ],
 )
 @triton.jit
-def _update_residual_stream_and_rsos_kernel(
+def _update_residual_stream_kernel(
     # fmt: off
-    residual_ptr, act_buf_ptr, act_rsos_ptr,
+    residual_ptr, act_buf_ptr, act_rsos_ptr, out_rsos_ptr, rmsnorm_w_ptr,
     residual_shape0: tl.constexpr, residual_shape1: tl.constexpr, residual_shape2: tl.constexpr,
     act_buf_shape0: tl.constexpr, act_buf_shape1: tl.constexpr, act_buf_shape2: tl.constexpr,
     act_rsos_shape0: tl.constexpr, act_rsos_shape1: tl.constexpr,
+    out_rsos_shape0: tl.constexpr, out_rsos_shape1: tl.constexpr,
+    rmsnorm_w_shape0: tl.constexpr,
     residual_stride0: tl.constexpr, residual_stride1: tl.constexpr, residual_stride2: tl.constexpr,
     act_buf_stride0: tl.constexpr, act_buf_stride1: tl.constexpr, act_buf_stride2: tl.constexpr,
     act_rsos_stride0: tl.constexpr, act_rsos_stride1: tl.constexpr,
+    out_rsos_stride0: tl.constexpr, out_rsos_stride1: tl.constexpr,
+    rmsnorm_w_stride0: tl.constexpr,
     BLOCKSIZE0: tl.constexpr, BLOCKSIZE1: tl.constexpr, BLOCKSIZE2: tl.constexpr,
     eps: tl.constexpr,
     # fmt: on
@@ -80,6 +92,9 @@ def _update_residual_stream_and_rsos_kernel(
     tl.static_assert(residual_shape1 == act_buf_shape1)
     tl.static_assert(residual_shape1 == act_rsos_shape1)
     tl.static_assert(residual_shape2 == act_buf_shape2)
+    tl.static_assert(residual_shape2 == rmsnorm_w_shape0)
+    tl.static_assert(out_rsos_shape0 == act_rsos_shape0)
+    tl.static_assert(out_rsos_shape1 == act_rsos_shape1)
 
     pid_b = tl.program_id(2)
     pid_t = tl.program_id(1)
@@ -103,6 +118,9 @@ def _update_residual_stream_and_rsos_kernel(
     rsos_off = off_b * act_rsos_stride0 + off_t * act_rsos_stride1
     act_rsos = tl.load(act_rsos_ptr + rsos_off, mask=(off_b < act_rsos_shape0) & (off_t < act_rsos_shape1))
 
+    rmsnorm_w_off = off_d * rmsnorm_w_stride0
+    rmsnorm_w = tl.load(rmsnorm_w_ptr + rmsnorm_w_off, mask=off_d < rmsnorm_w_shape0)
+
     inv_rms = tl.rsqrt(act_rsos / residual_shape2 + eps)
 
     residual += act_buf * inv_rms
@@ -113,18 +131,36 @@ def _update_residual_stream_and_rsos_kernel(
         mask=(off_b < residual_shape0) & (off_t < residual_shape1) & (off_d < residual_shape2),
     )
     tl.store(
-        act_rsos_ptr + rsos_off.reshape((BLOCKSIZE0, BLOCKSIZE1)),
+        act_buf_ptr + act_buf_off,
+        residual * rmsnorm_w,
+        mask=(off_b < act_buf_shape0) & (off_t < act_buf_shape1) & (off_d < act_buf_shape2),
+    )
+    out_rsos_off = off_b * out_rsos_stride0 + off_t * out_rsos_stride1
+    tl.atomic_add(
+        out_rsos_ptr + out_rsos_off.reshape((BLOCKSIZE0, BLOCKSIZE1)),
         (residual * residual).sum(-1),
-        mask=((off_b < act_rsos_shape0) & (off_t < act_rsos_shape1)).reshape((BLOCKSIZE0, BLOCKSIZE1)),
+        mask=((off_b < out_rsos_shape0) & (off_t < out_rsos_shape1)).reshape((BLOCKSIZE0, BLOCKSIZE1)),
     )
 
 
-def update_residual_stream_and_rsos(residual: Tensor, act_buf: Tensor, act_rsos: Tensor, eps: float):
+def update_residual_stream(
+    residual: Tensor,
+    act_buf: Tensor,
+    act_rsos: Tensor,
+    out_rsos: Tensor,
+    rmsnorm_w: Tensor,
+    eps: float,
+):
     grid_fn = lambda META: (
         triton.cdiv(residual.shape[2], META["BLOCKSIZE2"]),
         triton.cdiv(residual.shape[1], META["BLOCKSIZE1"]),
         triton.cdiv(residual.shape[0], META["BLOCKSIZE0"]),
     )
-    return launch[_update_residual_stream_and_rsos_kernel, grid_fn](
-        residual=residual, act_buf=act_buf, act_rsos=act_rsos, eps=float(eps)
+    return launch[_update_residual_stream_kernel, grid_fn](
+        residual=residual,
+        act_buf=act_buf,
+        act_rsos=act_rsos,
+        out_rsos=out_rsos,
+        rmsnorm_w=rmsnorm_w,
+        eps=float(eps),
     )
