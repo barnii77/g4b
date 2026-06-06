310d12112c0cadb10a7fb9d3d1858834fa1d5ee2
barnii77 <barnii77@barnii77.dev>
2026-06-06 19:02:21 +0200

    plan fa2

---
+++ g4b/kernels/fa2.py
@@ -0,0 +1,14 @@
+# TODO foreach KV group I should load all corresponding queries in a single thread-block so I can get a
+#  (g x d_k) @ (d_k x tile_T) tl.dot operation  -> much better arithmetic intensity than loading KV tiles for each
+#  query separately.
+# TODO parallelism over T
+# TODO try to make sure the tile layouts produced by triton minimize cross-warp reductions
+# TODO make sure I only do .max() and .sum() *reduces* outside of the loop across the T dim
+# TODO need an extra boolean (B,)-sized tensor that specifies whether each user is currently in prefill or decode. Then
+#  I have to use this information in the relevant prefill or decode FA kernels to efficiently partition work.
+# TODO I will also likely need to come up with fancy work partitioning strategies involving persistent kernels and
+#  blackwell-style work-stealing-ish dynamically-scheduled work assignment to deal with the highly heterogeneous nature
+#  of the decode phase especially but also prefill, since the context length between users differs greatly, likely
+#  following a long-tailed distribution.
+# TODO I need a temporary KV buffer for when the SWA window size is < chunked prefill size, from which I then memcpy to
+#  the actual KV cache (which is only 512 tokens long for SWA layers)
