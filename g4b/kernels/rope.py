683ae5de809a7624d9774360f5bf121e88a310a4
barnii77 <barnii77@barnii77.dev>
2026-06-14 13:25:44 +0200

    fuse rmsnorm finishing into rope for Q and K

--- g4b/kernels/rope.py
+++ g4b/kernels/rope.py
@@ -8,20 +8,32 @@ from g4b.kernels.utils import launch, default_bencher
 @triton.jit
 def _rope_tile_jfn(
     # fmt: off
-    x_ptr, x_mask,
+    x_ptr, x_rsos_ptr, x_rmsnorm_w_ptr, x_mask,
     sin, cos,
     offs_b, offs_h, offs_t, offs_k1, offs_k2,
     x_stride0: tl.constexpr, x_stride1: tl.constexpr, x_stride2: tl.constexpr, x_stride3: tl.constexpr,
+    x_rsos_stride0: tl.constexpr, x_rsos_stride1: tl.constexpr, x_rsos_stride2: tl.constexpr,
+    x_rmsnorm_w_stride0: tl.constexpr,
+    x_shape3: tl.constexpr,
+    rmsnorm_eps: tl.constexpr,
     # fmt: on
 ):
     start_of_head_ptrs = x_ptr + offs_b * x_stride0 + offs_h * x_stride1 + offs_t * x_stride2
     x1_ptrs = start_of_head_ptrs + offs_k1 * x_stride3
     x2_ptrs = start_of_head_ptrs + offs_k2 * x_stride3
-    k1 = tl.load(x1_ptrs, mask=x_mask)
-    k2 = tl.load(x2_ptrs, mask=x_mask)
+    x1 = tl.load(x1_ptrs, mask=x_mask)
+    x2 = tl.load(x2_ptrs, mask=x_mask)
 
-    y1 = cos * k1 - sin * k2
-    y2 = cos * k2 + sin * k1
+    rsos_ptrs = x_rsos_ptr + offs_b * x_rsos_stride0 + offs_h * x_rsos_stride1 + offs_t * x_rsos_stride2
+    rsos = tl.load(rsos_ptrs, mask=x_mask)
+    inv_rms = tl.rsqrt(rsos / x_shape3 + rmsnorm_eps)
+    w1 = tl.load(x_rmsnorm_w_ptr + offs_k1 * x_rmsnorm_w_stride0, mask=x_mask)
+    w2 = tl.load(x_rmsnorm_w_ptr + offs_k2 * x_rmsnorm_w_stride0, mask=x_mask)
+    x1 *= inv_rms * w1
+    x2 *= inv_rms * w2
+
+    y1 = cos * x1 - sin * x2
+    y2 = cos * x2 + sin * x1
 
     tl.store(x1_ptrs, y1, mask=x_mask)
     tl.store(x2_ptrs, y2, mask=x_mask)
@@ -83,12 +95,21 @@ def _cfg(
     # fmt: on
     key=[
         # fmt: off
-        "x_shape0", "x_shape1", "x_shape2", "x_shape3",
-        "cache_shape0", "cache_shape1", "cache_shape2", "cache_shape3",
+        "q_shape0", "q_shape1", "q_shape2", "q_shape3",
+        "k_shape0", "k_shape1", "k_shape2", "k_shape3",
+        "q_rsos_shape0", "q_rsos_shape1", "q_rsos_shape2",
+        "k_rsos_shape0", "k_rsos_shape1", "k_rsos_shape2",
+        "q_rmsnorm_w_shape0",
+        "k_rmsnorm_w_shape0",
         "cache_offsets_shape0",
-        "x_stride0", "x_stride1", "x_stride2", "x_stride3",
-        "cache_stride0", "cache_stride1", "cache_stride2", "cache_stride3",
+        "q_stride0", "q_stride1", "q_stride2", "q_stride3",
+        "k_stride0", "k_stride1", "k_stride2", "k_stride3",
+        "q_rsos_stride0", "q_rsos_stride1", "q_rsos_stride2",
+        "k_rsos_stride0", "k_rsos_stride1", "k_rsos_stride2",
+        "q_rmsnorm_w_stride0",
+        "k_rmsnorm_w_stride0",
         "cache_offsets_stride0",
+        "rmsnorm_eps",
         # fmt: on
     ],
     do_bench=default_bencher,
@@ -98,29 +119,46 @@ def _cfg(
 def _apply_rope_kernel(
     # fmt: off
     q_ptr, k_ptr, rope_freqs_ptr, time_dim_offsets_ptr,
+    q_rsos_ptr, k_rsos_ptr, q_rmsnorm_w_ptr, k_rmsnorm_w_ptr,
     q_shape0: tl.constexpr, q_shape1: tl.constexpr, q_shape2: tl.constexpr, q_shape3: tl.constexpr,
     k_shape0: tl.constexpr, k_shape1: tl.constexpr, k_shape2: tl.constexpr, k_shape3: tl.constexpr,
     rope_freqs_shape0: tl.constexpr,
+    q_rsos_shape0: tl.constexpr, q_rsos_shape1: tl.constexpr, q_rsos_shape2: tl.constexpr,
+    k_rsos_shape0: tl.constexpr, k_rsos_shape1: tl.constexpr, k_rsos_shape2: tl.constexpr,
+    q_rmsnorm_w_shape0: tl.constexpr,
+    k_rmsnorm_w_shape0: tl.constexpr,
     time_dim_offsets_shape0: tl.constexpr,
     q_stride0: tl.constexpr, q_stride1: tl.constexpr, q_stride2: tl.constexpr, q_stride3: tl.constexpr,
     k_stride0: tl.constexpr, k_stride1: tl.constexpr, k_stride2: tl.constexpr, k_stride3: tl.constexpr,
     rope_freqs_stride0: tl.constexpr,
+    q_rsos_stride0: tl.constexpr, q_rsos_stride1: tl.constexpr, q_rsos_stride2: tl.constexpr,
+    k_rsos_stride0: tl.constexpr, k_rsos_stride1: tl.constexpr, k_rsos_stride2: tl.constexpr,
+    q_rmsnorm_w_stride0: tl.constexpr,
+    k_rmsnorm_w_stride0: tl.constexpr,
     time_dim_offsets_stride0: tl.constexpr,
     BLOCKSIZE0: tl.constexpr, BLOCKSIZE1: tl.constexpr, BLOCKSIZE2: tl.constexpr, BLOCKSIZE3: tl.constexpr,
+    rmsnorm_eps: tl.constexpr,
     # fmt: on
 ):
     tl.static_assert(k_shape3 // 2 == rope_freqs_shape0)
     tl.static_assert(k_shape0 == time_dim_offsets_shape0)
     tl.static_assert(k_shape0 == q_shape0)
-    tl.static_assert(k_shape1 == q_shape1)
     tl.static_assert(k_shape2 == q_shape2)
     tl.static_assert(k_shape3 == q_shape3)
+    tl.static_assert(q_rsos_shape0 == q_shape0)
+    tl.static_assert(q_rsos_shape1 == q_shape1)
+    tl.static_assert(q_rsos_shape2 == q_shape2)
+    tl.static_assert(k_rsos_shape0 == k_shape0)
+    tl.static_assert(k_rsos_shape1 == k_shape1)
+    tl.static_assert(k_rsos_shape2 == k_shape2)
+    tl.static_assert(q_rmsnorm_w_shape0 == q_shape3)
+    tl.static_assert(k_rmsnorm_w_shape0 == k_shape3)
 
     tl.static_assert(k_shape3 % 2 == 0)  # so my split logic makes sense
     k_split_size: tl.constexpr = k_shape3 // 2
 
     pid_b = tl.program_id(2)
-    n_pid_h: tl.constexpr = tl.cdiv(k_shape1, BLOCKSIZE1)
+    n_pid_h: tl.constexpr = tl.maximum(tl.cdiv(k_shape1, BLOCKSIZE1), tl.cdiv(q_shape1, BLOCKSIZE1))
     pid_h = pid_b % n_pid_h
     pid_b //= n_pid_h
     pid_t = tl.program_id(1)
@@ -144,31 +182,61 @@ def _apply_rope_kernel(
     cos = tl.cos(theta)
 
     k_mask = (offs_b < k_shape0) & (offs_h < k_shape1) & (offs_t < k_shape2) & (offs_k1 < k_split_size)
+    q_mask = (offs_b < q_shape0) & (offs_h < q_shape1) & (offs_t < q_shape2) & (offs_k1 < k_split_size)
 
     # fmt: off
     _rope_tile_jfn(
-        k_ptr, k_mask,
+        k_ptr, k_rsos_ptr, k_rmsnorm_w_ptr, k_mask,
         sin, cos,
         offs_b, offs_h, offs_t, offs_k1, offs_k2,
         k_stride0, k_stride1, k_stride2, k_stride3,
+        k_rsos_stride0, k_rsos_stride1, k_rsos_stride2,
+        k_rmsnorm_w_stride0,
+        k_shape3,
+        rmsnorm_eps,
     )
     _rope_tile_jfn(
-        q_ptr, k_mask,
+        q_ptr, q_rsos_ptr, q_rmsnorm_w_ptr, q_mask,
         sin, cos,
         offs_b, offs_h, offs_t, offs_k1, offs_k2,
         q_stride0, q_stride1, q_stride2, q_stride3,
+        q_rsos_stride0, q_rsos_stride1, q_rsos_stride2,
+        q_rmsnorm_w_stride0,
+        q_shape3,
+        rmsnorm_eps,
     )
     # fmt: on
 
 
-def apply_rope(q: Tensor, k: Tensor, rope_freqs: Tensor, time_dim_offsets: Tensor):
+def apply_rope(
+    q: Tensor,
+    k: Tensor,
+    rope_freqs: Tensor,
+    time_dim_offsets: Tensor,
+    q_rsos: Tensor,
+    k_rsos: Tensor,
+    q_rmsnorm_w: Tensor,
+    k_rmsnorm_w: Tensor,
+    rmsnorm_eps: float,
+):
     assert rope_freqs.shape[-1] == k.shape[-1] // 2
     grid_fn = lambda META: (
         triton.cdiv(k.shape[3] // 2, META["BLOCKSIZE3"]),
         triton.cdiv(k.shape[2], META["BLOCKSIZE2"]),
-        triton.cdiv(k.shape[1], META["BLOCKSIZE1"]) * triton.cdiv(k.shape[0], META["BLOCKSIZE0"]),
+        max(triton.cdiv(q.shape[1], META["BLOCKSIZE1"]), triton.cdiv(k.shape[1], META["BLOCKSIZE1"]))
+        * triton.cdiv(k.shape[0], META["BLOCKSIZE0"]),
     )
-    return launch[_apply_rope_kernel, grid_fn](q=q, k=k, rope_freqs=rope_freqs, time_dim_offsets=time_dim_offsets)
+    return launch[_apply_rope_kernel, grid_fn](
+        q=q,
+        k=k,
+        rope_freqs=rope_freqs,
+        time_dim_offsets=time_dim_offsets,
+        q_rsos=q_rsos,
+        k_rsos=k_rsos,
+        q_rmsnorm_w=q_rmsnorm_w,
+        k_rmsnorm_w=k_rmsnorm_w,
+        rmsnorm_eps=float(rmsnorm_eps),
+    )
 
 
 # Only used once during model loading
