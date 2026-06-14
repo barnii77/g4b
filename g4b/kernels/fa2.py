fb51db0311d64d70058de1a6a4d5e3a5fe37c9e9
barnii77 <barnii77@barnii77.dev>
2026-06-14 16:47:51 +0200

    todos

--- g4b/kernels/fa2.py
+++ g4b/kernels/fa2.py
@@ -1,9 +1,10 @@
 # TODO foreach KV group I should load all corresponding queries in a single thread-block so I can get a
-#  (g x d_k) @ (d_k x tile_T) tl.dot operation  -> much better arithmetic intensity than loading KV tiles for each
+#  ((g*tile_T1) x d_k) @ (d_k x tile_T2) tl.dot operation -> better arithmetic intensity than loading KV tiles for each
 #  query separately.
 # TODO parallelism over T
 # TODO try to make sure the tile layouts produced by triton minimize cross-warp reductions
-# TODO make sure I only do .max() and .sum() *reduces* outside of the loop across the T dim
+# TODO make sure I only do .max() and .sum() *reduces* outside of the loop across the T dim...
+#  WAIT ACTUALLY, maybe this is bad because it increases register pressure?
 # TODO need an extra boolean (B,)-sized tensor that specifies whether each user is currently in prefill or decode. Then
 #  I have to use this information in the relevant prefill or decode FA kernels to efficiently partition work.
 # TODO I will also likely need to come up with fancy work partitioning strategies involving persistent kernels and
