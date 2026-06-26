6e46367ed61fec66895b9608524954dcebbb07b5
barnii77 <barnii77@barnii77.dev>
2026-06-26 09:10:33 +0200

    update __init__.py

--- g4b/__init__.py
+++ g4b/__init__.py
@@ -2,11 +2,29 @@ from . import _torch_stub
 from . import config
 from . import device
 from . import gguf
+from . import interaction_generator
 from . import lifecycle
+from . import protocol
 from . import scheduler
+from . import serve
 from . import tensor
+from . import tokenizer
 from . import utils
 from . import kernels
 from . import models
 
-__all__ = ["config", "device", "gguf", "lifecycle", "scheduler", "tensor", "utils", "kernels", "models"]
+__all__ = [
+    "config",
+    "device",
+    "gguf",
+    "interaction_generator",
+    "lifecycle",
+    "protocol",
+    "scheduler",
+    "serve",
+    "tensor",
+    "tokenizer",
+    "utils",
+    "kernels",
+    "models",
+]
